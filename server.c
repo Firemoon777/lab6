e3639b75843b324270986d5f1359de4b0140350f
Voldemar <firemoon@icloud.com>
2017-05-29 00:48:15 +0300

    worker implemented

--- server.c
+++ server.c
@@ -1,41 +1,103 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define BUFFER_SIZE 80
+#define BUFFER_SIZE 1024
 
 int socket_fd;
 
 void worker() {
 	int client_fd;
 	struct sockaddr_in client;
-	socklen_t client_len;
+	socklen_t client_len = sizeof(client);
 	char buf[BUFFER_SIZE];
 	int buf_len;
-	
-	client_len = sizeof(client);
+	DIR* dir;
+	struct dirent* dirent;
+	size_t d_name_len, i;
 
 	while(1) {
-		client_fd = accept(socket_fd, (struct socketaddr*)&client, &client_len);
+		client_fd = accept(socket_fd, 
+				(struct socketaddr*)&client,
+			 	&client_len);
 		if(client_fd < 0) {
 			perror("accept");
 			continue;
 		}
-		fprintf(stderr, "accepted");
+		fprintf(stderr, "accepted\n");
 
-		while((buf_len = read(client_fd, buf, BUFFER_SIZE)) > 0) {
-			write(client_fd, buf, buf_len);
-		}
-
+		/* buffered input? */
+		/* one dirent per one line */
+		buf_len = read(client_fd, buf, BUFFER_SIZE);
 		if(buf_len < 0) {
 			perror("read");
+			close(client_fd);
+			continue;
 		}
+		if(buf[buf_len-1] == '\n') 
+			buf_len--;
+		if(buf[buf_len-1] == '\r') 
+			buf_len--;
+		buf[buf_len] = '\0';
+		fprintf(stderr, "request: \"%s\"\n", buf);
 
+		/* try to open dir */
+		dir = opendir(buf);
+		if(dir == NULL) {
+			/* error :C */
+			char* str = strerror(errno);
+			write(client_fd, str, strlen(str));
+			write(client_fd, "\r\n", 2);
+			close(client_fd);
+			continue;
+		}
+		/* success */
+		write(client_fd, "OK\r\n", 4);
+
+		/* reading dirents */
+		/* set errno to detect end of dir */
+		errno = 0;
+		while(1) {
+			dirent = readdir(dir);
+			/* error and end of dir handling */
+			if(dirent == NULL) {
+				if(errno == 0) {
+					/* end of dir found */
+					break;
+				} else {
+					/* WTF */
+					/* error handling */
+
+					/* continue reading */
+					errno = 0;
+					continue;
+				}
+			}
+			/* output dirent name */
+			/* remove \n symbols */
+			d_name_len = strlen(dirent->d_name);
+			for(i = 0; i < d_name_len; i++) {
+				if(dirent->d_name[i] == '\n') {
+					dirent->d_name[i] = '?';
+				}
+			}
+			
+			write(client_fd, dirent->d_name, d_name_len);
+			write(client_fd, "\r\n", 2);
+
+		}
+	
+
+		/* close everything */
+		closedir(dir);
 		close(client_fd);
-		fprintf(stderr, "closed");
+		fprintf(stderr, "closed\n");
 	}
 }
 
@@ -55,7 +117,9 @@ int main() {
 	server.sin_port = htons(58015);
 
 	/* bind */
-	if(bind(socket_fd, (struct sockaddr*)&server, sizeof(server)) < 0) {
+	if(bind(socket_fd, 
+			(struct sockaddr*)&server, 
+			sizeof(server)) < 0) {
 		perror("bind");
 		return 2;
 	}
